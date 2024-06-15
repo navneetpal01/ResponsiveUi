b0932cb2c81de346f3841364632c940624132ff2
navneet <navneetpalgithub@gmail.com>
2024-06-15 18:19:18 +0530

    Mars

--- app/src/main/java/com/example/responsiveapp/ui/WindowSizeClass.kt
+++ app/src/main/java/com/example/responsiveapp/ui/WindowSizeClass.kt
@@ -1,13 +1,38 @@
 package com.example.responsiveapp.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalConfiguration
+
 
 sealed class WindowSize(
-    val
-){
+    val size: Int
+) {
 
+    data class Small(val smallSize: Int) : WindowSize(size = smallSize)
+    data class Medium(val mediumSize: Int) : WindowSize(size = mediumSize)
+    data class Compact(val compactSize: Int) : WindowSize(size = compactSize)
+    data class Large(val largeSize: Int) : WindowSize(size = largeSize)
 
+}
 
+data class WindowSizeClass(
+    val height: WindowSize,
+    val width: WindowSize
+)
 
+@Composable
+fun rememberWindowSize(): WindowSizeClass {
+
+    val config = LocalConfiguration.current
+    val width by remember(config) { mutableStateOf(config.screenWidthDp) }
+    val height by remember(config){ mutableStateOf(config.screenHeightDp) }
+
+    val windowWidthClass = when{
+
+    }
 
 }
 
